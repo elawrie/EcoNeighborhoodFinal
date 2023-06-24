393edf7ba98f12ec4c17d52c2ab4f580e40e713e
Evelyn Lawrie <lawrie@chapman.edu>
2023-06-24 18:44:35 -0400

    comienza a conectar el frontend y el backend

--- ProyectoIngWeb/src/app/componentes/crearcuenta/crearcuenta.component.ts
+++ ProyectoIngWeb/src/app/componentes/crearcuenta/crearcuenta.component.ts
@@ -15,6 +15,32 @@ export class CrearcuentaComponent implements OnInit {
     password2: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$')])
   })
 
+  // FIXME: esta funcion produce errores de jquery para conectar frontend y backend con registar un usuario en la bd
+  // EnviarDatos(): void {
+  //   console.log("Button clicked!");
+  //   const email: string = $("#email").val() as string;
+  //   const password: string = $("#password").val() as string;
+  //   const puntos: number = 0;
+  //   const url: string = "http://localhost:3000";
+  //   $.ajax({
+  //     data: JSON.stringify({ "email": email, "password": password, "puntos": puntos }),
+  //     contentType: "application/json",
+  //     type: "PUT",
+  //     dataType: "json",
+  //     url: url + "/registro",
+  //   })
+  //     .done(function (data: any, textStatus: string, jqXHR: JQueryXHR) {
+  //       if (console && console.log) {
+  //         console.log(JSON.stringify(data));
+  //       }
+  //     })
+  //     .fail(function (jqXHR: JQueryXHR, textStatus: string, errorThrown: any) {
+  //       if (console && console.log) {
+  //         console.log("La solicitud a fallado: " + textStatus);
+  //       }
+  //     });
+  // }
+
   loginUser() {
     console.warn(this.loginForm.value);
     this.loginForm.reset();
