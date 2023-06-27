584eddfe903399ac88ddb4637b27eca1a6741563
Evelyn Lawrie <lawrie@chapman.edu>
2023-06-26 20:10:39 -0400

    comienza a agregar el formulario

--- ProyectoIngWeb/src/app/componentes/formulario/formulario.component.ts
+++ ProyectoIngWeb/src/app/componentes/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Form, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  title = 'Angular Reactive Form';
+  formulario = new FormGroup({
+    reciclaje: new FormControl('', [Validators.required]),
+  })
 
+  userForm() {
+    console.warn(this.formulario.value);
+    this.formulario.reset();
+  }
 }
